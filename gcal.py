a28c0e67113e69b3cdfff58286bd35cfa0f467b6
Max <max-nicolay@gmx.de>
2022-08-06 00:50:19 +0200

    one bug left..
        suche zeilen in main mit  ".Value"  ...

--- gcal.py
+++ gcal.py
@@ -1,7 +1,13 @@
+"""
+author: max nicolay
+date: 06.08.22
+"""
+
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 from datetime import datetime as dt
 from datetime import timedelta
+
 calendar_haushalt = GoogleCalendar("regg900o16pp20bc9fcbnud2gg@group.calendar.google.com")
 calendar_arbeit= GoogleCalendar('raspmaxcal@gmail.com')
 
@@ -13,37 +19,27 @@ def add_termin(titel, start, end):
     event = Event(titel, start, end)
     calendar_haushalt.add_event(event)
 
-# noch letzte Woche automatisch einstellen hinzufügen.. start aktuelle kw-1 bis ende
-#allgemeine Funktion für Zeitraum
-def check_events(time_start, time_end,name_of_job):
-#    for i in calendar_arbeit.get_events(time_start, time_end):
-
-    for i in calendar_arbeit.get_events():
-            print(i.summary)  
-#
-#    #        if name_of_job in i.summary:
-#            print("Termin'{}'.".format(name_of_job))
-#            return (i.start.isocalendar()[1])
-#        else:
-#            return(False)
-
 def return_event_on_day(datetime_date):
+    """
+    returns first event at the given day or None if there's no event
+    """
     start= datetime.datetime.combine(datetime_date,datetime.time(hour=0))
     end = start+ datetime.timedelta(hours=23,minutes=59)
     for i in calendar_arbeit.get_events(time_min=start,time_max=end):
         if i is None:
-            return False
+            return None
         else:
             return i
 
 
-def get_events_month(month:int,query="",year=None,order_by="startTime"):
+def get_events_month(month:int,query="",year=None,order_by="startTime",calendar=calendar_arbeit):
     """
         returns the events of calendar_arbeit for the given month, optional filtered by query parameter.
-    
+
         month : int     the month for which you want the events (current year)
         query : str     an optional query parameter (summary field of calender) caseinsensitiv 
         year  : int     default year is current year, if you want to get events from an recent year use this parameter
+
     """
 
     t = dt.today()
@@ -55,12 +51,10 @@ def get_events_month(month:int,query="",year=None,order_by="startTime"):
     end_of_month = dt(year,month+1,1) - timedelta(days=1)
 
 
-#    for i in calendar_arbeit.get_events(time_start, time_end):
     if query:
-        x = calendar_arbeit.get_events( beginn_of_month, end_of_month,query=query,order_by=order_by,single_events=True)
+        x = calendar.get_events( beginn_of_month, end_of_month,query=query,order_by=order_by,single_events=True)
     else:
-        x = calendar_arbeit.get_events( beginn_of_month, end_of_month,order_by=order_by,single_events=True)
+        x = calendar.get_events( beginn_of_month, end_of_month,order_by=order_by,single_events=True)
 
     return list(x)
-    
-check_events(start,end,"janet")
+
--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import uno # get the uno component context from the PyUNO runtime
 import datetime as dt
 from gcal import *
 import atexit
-
-
 import signal
 import sys
 
+ODS_TEMPLATE = "ods_files/template/template.ods"  # path to libreoffice calc file
+# relevant columns and cells of the sheet
+COL_START = "B"
+COL_END = "C"
+COL_NOTE = "E"
+COL_DURATION = "D"
+CELL_MONTH_YEAR = "E7"
+CELL_PROJECT_NAME = "D7"
 
 
 def clean_exit():
@@ -17,30 +23,22 @@ def clean_exit():
     except EOFError:
         print(" ")
 
-        
-atexit.register(clean_exit)
 
 def signal_handler(sig, frame):
     print("Please use ctrl-d to exit!")
 
 
+atexit.register(clean_exit)
 signal.signal(signal.SIGINT, signal_handler)
-#signal.pause()
 
 
+choice= input("Run libreoffice? [y/n] default yes:   ")
 
-
-
-ODS_TEMPLATE = "ods_files/template/template.ods"
-
-choice= input("Run libreoffice? [y/n] default yes: ")
 if not (choice == "n" or choice=="no"):
 # open sheet in libreoffice...
     cmd=f'soffice {ODS_TEMPLATE} --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager"'
     os.popen(cmd) # todo... besser iwie forken oder so? kp
-
-
-    input("LibreOffice is opening 'template.ods', continue with enter")
+    input("confirm with Enter if libreoffice is ready..")
 
     localContext = uno.getComponentContext() # create the UnoUrlResolver
     resolver = localContext.ServiceManager.createInstanceWithContext(
@@ -53,27 +51,7 @@ if not (choice == "n" or choice=="no"):
     # access the active sheet
     active_sheet = model.CurrentController.ActiveSheet
 
-## exampels..
-# access cell C4
-#cell1 = active_sheet.getCellRangeByName("C4")
-# set text inside
-#cell1.String = "Hello world"
-# other example with a value
-#cell2 = active_sheet.getCellRangeByName("E6")
-#cell2.Value = cell2.Value + 1
 
-
-## input zeiten der tage geht von Zeile 11 = 1 bis 41 = 31 
-COL_START = "B"
-COL_END = "C"
-COL_NOTE = "E"
-COL_DURATION = "D"
-
-CELL_MONTH_YEAR = "E7"
-CELL_PROJECT_NAME = "D7"
-
-
-## modul functions, use this with care!! better use the  userfunctions below!
 
 def _add_times(day_of_month: int, start: str, end: str, note=""):
 
@@ -98,15 +76,9 @@ def _add_times(day_of_month: int, start: str, end: str, note=""):
 
     if c_start.String != "" :
         input("nicht leer ERROR")
-#        print("old=" ,c_note.String)
-#        c_note.String = f"{c_start.String}-{c_end.String};"
 
     c_start.String = start
     c_end.String = end
-
-#    if old_val:
-#            active_sheet.getCellRangeByName("D" + str(day_of_month+10)).String = int(old_val+
-
 
     if note:
         c_note.String = note
@@ -130,21 +102,16 @@ def _add_values(day_of_month: int, start: str, end: str, duration_plus: int):
 
     # if there's already an appoinment, add duration,
     if c_start.String != "" :
-        c_duration.String = str(round( float(c_duration.String.replace(',', ''))  +  duration_plus,2) )
+        c_duration.Value = round( float(c_duration.String.replace(',', ''))  +  duration_plus,2) 
+
+        c_duration.Value = round( c.duration.Value  +  duration_plus,2) 
         c_start.String = "siehe"
         c_end.String = "rechts"
 
     else:
-        c_duration.String = str( duration_plus )
+        c_duration.Value =  duration_plus
         c_start.String = start
         c_end.String = end
-
-
-#    if old_val:
-#            active_sheet.getCellRangeByName("D" + str(day_of_month+10)).String = int(old_val+
-
-
-
 
 
 def _add_from_gcal(month:int , query=""):
@@ -156,7 +123,7 @@ def _add_from_gcal(month:int , query=""):
     # save all events in dict, in case there is more than one appoinment per day 
     day_dict = dict()
     for i in events:
-        print( f"{i.start.day}.{i.start.month} {i.start.hour}:{i.start.minute} - {i.end.hour}:{i.end.minute}\t{i.summary}")
+        print( f"{i.start.day}.{i.start.month} {i.start.hour}:{i.start.minute:02} - {i.end.hour}:{i.end.minute:02}\t{i.summary}")
 
         # if key exits append appointment to list
         if f"{i.start.day}.{i.start.month}" in day_dict.keys():
@@ -172,38 +139,18 @@ def _add_from_gcal(month:int , query=""):
         input("import next appoinment  with enter...\n")
         if len(i) > 1:
             for j in i:
-                _add_values(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute}",end= f"{j.end.hour}:{j.end.minute}", duration_plus= (j.end -j.start).total_seconds()/3600 )
+                _add_values(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute:02}",end= f"{j.end.hour}:{j.end.minute:02}", duration_plus= (j.end -j.start).total_seconds()/3600 )
 
         # if just one appoinment in list(= just one appoinment at this day), add just the times to sheet, and let libreoffice calculate the duration field 
         else:
             for j in i:
-                _add_times(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute}",end= f"{j.end.hour}:{j.end.minute}" ,note=j.summary)
-
-
-
-#        print( f"{i.start.day}.{,i.start.month} {i.start.hour}:{i.start.minute} - {i.end.hour}:{i.end.minute}\t{i.summary}")
-
-#        if f"{i.start.day}.{i.start.month}" in day_dict.keys():
-#            day_dict[f"{i.start.day}.{i.start.month}"][0] += (i.end -i.start).total_seconds()/3600
-#
-#        else:
-#            day_dict[f"{i.start.day}.{i.start.month}"] = list()
-#            day_dict[f"{i.start.day}.{i.start.month}"][0] = (i.end -i.start).total_seconds()/3600
-#            day_dict[f"{i.start.day}.{i.start.month}"][1]= list()
-#            day_dict[f"{i.start.day}.{i.start.month}"][1]
-
-#    print(day_dict)
-#        time.sleep(0.5)
-
-
-
-
-
+                _add_times(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute:02}",end= f"{j.end.hour}:{j.end.minute:02}" ,note=j.summary)
 
 
 ## user functions
 
 def add_from_gcal(query="Nachhilfe"):
+
     t= dt.today()
     last_month = dt(t.year,t.month,1) - timedelta(days=1)
     text1= f'Add appoinments for last month {last_month.strftime("%h%m")}  [y/n] default:yes'
@@ -211,14 +158,17 @@ def add_from_gcal(query="Nachhilfe"):
     choice= choice.lower()
     month = last_month
 
+
     # if user wants a differnt month.. 
     # TODO add special case user wants a month from an old year (unlikely)
     if (choice=="n" or choice == "no"):
         month = input("Please enter the month as integer:")
         _add_from_gcal(int(month),query)
+        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{month:02}/{t.year})"
 
     else:
         _add_from_gcal(month.month,query)
+        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{month.month:02}/{t.year})"
 
 
 
