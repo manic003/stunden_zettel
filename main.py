574c74560def42f2a327eca695be184119bb854e
Max <max-nicolay@gmx.de>
2022-08-06 10:51:24 +0200

    fertig

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def _add_times(day_of_month: int, start: str, end: str, note=""):
     c_note = active_sheet.getCellRangeByName(COL_NOTE + str(day_of_month+10))
 
     if c_start.String != "" :
-        input("nicht leer ERROR")
+        input("ERROR nicht behandelter FALL!! Das Skript überschreibt jetzt Werte die nicht berücksichtigt werden!!!")
 
     c_start.String = start
     c_end.String = end
@@ -97,21 +97,22 @@ def _add_values(day_of_month: int, start: str, end: str, duration_plus: int):
     c_note = active_sheet.getCellRangeByName(COL_NOTE + str(day_of_month+10))
     c_duration = active_sheet.getCellRangeByName(COL_DURATION + str(day_of_month+10))
 
-    #append time note to notes field
-    c_note.String =  c_note.String + f"{start}-{end};"
-
     # if there's already an appoinment, add duration,
     if c_start.String != "" :
-        c_duration.Value = round( float(c_duration.String.replace(',', ''))  +  duration_plus,2) 
-
-        c_duration.Value = round( c.duration.Value  +  duration_plus,2) 
+        c_duration.Value = round( c_duration.Value  +  duration_plus,2)
         c_start.String = "siehe"
         c_end.String = "rechts"
+
+
+        #append time note to notes field
+        c_note.String =  c_note.String + f";{start}-{end}"
 
     else:
         c_duration.Value =  duration_plus
         c_start.String = start
         c_end.String = end
+        #append time note to notes field
+        c_note.String = f"{start}-{end}"
 
 
 def _add_from_gcal(month:int , query=""):
@@ -123,7 +124,6 @@ def _add_from_gcal(month:int , query=""):
     # save all events in dict, in case there is more than one appoinment per day 
     day_dict = dict()
     for i in events:
-        print( f"{i.start.day}.{i.start.month} {i.start.hour}:{i.start.minute:02} - {i.end.hour}:{i.end.minute:02}\t{i.summary}")
 
         # if key exits append appointment to list
         if f"{i.start.day}.{i.start.month}" in day_dict.keys():
@@ -136,15 +136,27 @@ def _add_from_gcal(month:int , query=""):
 
     # for all days iterate through the appoinment list
     for i in day_dict.values():
-        input("import next appoinment  with enter...\n")
         if len(i) > 1:
             for j in i:
-                _add_values(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute:02}",end= f"{j.end.hour}:{j.end.minute:02}", duration_plus= (j.end -j.start).total_seconds()/3600 )
+                text= f"add --> {j.start.day:02}.{j.start.month:02} {j.start.hour:02}:{j.start.minute:02}-{j.end.hour:02}:{j.end.minute:02}\t{j.summary} ?  "
+                choice = input(text)
+                choice = choice.lower()
+                if not (choice == "no" or choice == "n"):
+                    _add_values(day_of_month=j.start.day, start=f"{j.start.hour:02}:{j.start.minute:02}",end= f"{j.end.hour:02}:{j.end.minute:02}", duration_plus= (j.end -j.start).total_seconds()/3600 )
+                else:
+                    print("skipped this appoinment ")
 
         # if just one appoinment in list(= just one appoinment at this day), add just the times to sheet, and let libreoffice calculate the duration field 
         else:
             for j in i:
-                _add_times(day_of_month=j.start.day, start=f"{j.start.hour}:{j.start.minute:02}",end= f"{j.end.hour}:{j.end.minute:02}" ,note=j.summary)
+                text= f"add --> {j.start.day:02}.{j.start.month:02} {j.start.hour:02}:{j.start.minute:02}-{j.end.hour:02}:{j.end.minute:02}\t{j.summary} ?  "
+                choice = input(text)
+                choice = choice.lower()
+                if not (choice == "no" or choice == "n"):
+                    _add_times(day_of_month=j.start.day, start=f"{j.start.hour:02}:{j.start.minute:02}",end= f"{j.end.hour:02}:{j.end.minute:02}" ,note=j.summary)
+
+                else:
+                    print("skipped this appoinment ")
 
 
 ## user functions
@@ -153,7 +165,7 @@ def add_from_gcal(query="Nachhilfe"):
 
     t= dt.today()
     last_month = dt(t.year,t.month,1) - timedelta(days=1)
-    text1= f'Add appoinments for last month {last_month.strftime("%h%m")}  [y/n] default:yes'
+    text1= f'Add appoinments for last month {last_month.strftime("%h%m")}?  [y/n] default:yes    '
     choice= input(text1)
     choice= choice.lower()
     month = last_month
@@ -162,13 +174,13 @@ def add_from_gcal(query="Nachhilfe"):
     # if user wants a differnt month.. 
     # TODO add special case user wants a month from an old year (unlikely)
     if (choice=="n" or choice == "no"):
-        month = input("Please enter the month as integer:")
+        month = input("Please enter the month as integer: ")
         _add_from_gcal(int(month),query)
-        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{month:02}/{t.year})"
+        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{int(month):02}/{t.year}"
 
     else:
         _add_from_gcal(month.month,query)
-        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{month.month:02}/{t.year})"
+        active_sheet.getCellRangeByName(CELL_MONTH_YEAR).String = f"{month.month:02}/{t.year}"
 
 
 
